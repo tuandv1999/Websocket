0c4bc30184fbfd7a7910179b12e24056fde44431
tuandv40 <tuandv40>
2022-10-26 14:32:06 +0700

    socket

---
+++ src/main/java/com/example/websotket/WebsotketApplication.java
@@ -0,0 +1,13 @@
+package com.example.websotket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsotketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsotketApplication.class, args);
+    }
+
+}
